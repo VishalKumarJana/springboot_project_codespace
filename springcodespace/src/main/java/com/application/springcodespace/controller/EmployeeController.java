4b0d32bd3e82e011be44f3cad34a2508a693b8ab
Vishal Kumar Jana <kumar.jana1999@gmail.com>
2026-02-25 10:01:21 +0000

    Modified the configuration

--- springcodespace/src/main/java/com/application/springcodespace/controller/EmployeeController.java
+++ springcodespace/src/main/java/com/application/springcodespace/controller/EmployeeController.java
@@ -1,13 +1,13 @@
 package com.application.springcodespace.controller;
 
-// import com.application.springcodespace.entity.Employee;
-// import com.application.springcodespace.repo.EmployeeRepo;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
+import com.application.springcodespace.entity.Employee;
+import com.application.springcodespace.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-// import java.util.List;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -16,46 +16,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("/api/v1/employees")
 public class EmployeeController {
 
-    // @Autowired
-    // private EmployeeRepo employeeRepo;
+    @Autowired
+    private EmployeeRepo employeeRepo;
 
-    // @GetMapping
-    // public List<Employee> getAllEmployees(){
-    //     return employeeRepo.findAll();
-    // }
+    @GetMapping("/AllEmployee")
+    public List<Employee> getAllEmployees(){
+        return employeeRepo.findAll();
+    }
 
-    // @PostMapping
-    // public Employee createEmployee(@RequestBody Employee employee){
-    //     return employeeRepo.save(employee);
-    // }
+    @PostMapping("/addEmployee")
+    public Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepo.save(employee);
+    }
 
-    // @GetMapping("{employeeId}")
-    // public ResponseEntity<Employee> getEmployeeById(@PathVariable long employeeId) {
-    //     Employee employee = employeeRepo.findById(employeeId).orElseThrow(() -> new RuntimeException("Employee not exist with id :" +employeeId));
-    //     return ResponseEntity.ok(employee);
-    // }
+    @GetMapping("{employeeId}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long employeeId) {
+        Employee employee = employeeRepo.findById(employeeId).orElseThrow(() -> new RuntimeException("Employee not exist with id :" +employeeId));
+        return ResponseEntity.ok(employee);
+    }
 
-    // @PutMapping("{employeeId}")
-    // public ResponseEntity<Employee> updateEmployee(@PathVariable long employeeId,@RequestBody Employee employeeDetails){
-    //     Employee updateEmp = employeeRepo.findById(employeeId).orElseThrow(() -> new RuntimeException("Employee not exist with id :" +employeeId));
-    //     updateEmp.setFirstName(employeeDetails.getFirstName());
-    //     updateEmp.setLastName(employeeDetails.getLastName());
-    //     updateEmp.setEmailId(employeeDetails.getEmailId());
+    @PutMapping("{employeeId}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long employeeId,@RequestBody Employee employeeDetails){
+        Employee updateEmp = employeeRepo.findById(employeeId).orElseThrow(() -> new RuntimeException("Employee not exist with id :" +employeeId));
+        updateEmp.setFirstName(employeeDetails.getFirstName());
+        updateEmp.setLastName(employeeDetails.getLastName());
+        updateEmp.setEmailId(employeeDetails.getEmailId());
 
-    //     return ResponseEntity.ok(employeeRepo.save(updateEmp));
-    // }
+        return ResponseEntity.ok(employeeRepo.save(updateEmp));
+    }
 
-    // @DeleteMapping("{employeeId}")
-    // public ResponseEntity<Employee> deleteEmployee(@PathVariable long employeeId){
-    //     Employee employee = employeeRepo.findById(employeeId).orElseThrow(() -> new RuntimeException("Employee not exist with id :" +employeeId));
-    //     employeeRepo.delete(employee);
-    //     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    // }
+    @DeleteMapping("{employeeId}")
+    public ResponseEntity<Employee> deleteEmployee(@PathVariable long employeeId){
+        Employee employee = employeeRepo.findById(employeeId).orElseThrow(() -> new RuntimeException("Employee not exist with id :" +employeeId));
+        employeeRepo.delete(employee);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 
     @GetMapping("/path")
     public String getMethodName() {
-        return "Hello message";
+        return "Hello CodeSpace message";
     }
-    
 
 }
